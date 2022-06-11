fe565f743669b44399f37bbfb1e4371402d3f4ba
Sami Ali <samibaloch13@hotmail.com>
2022-06-10 20:40:05 -0700

    commiting-operations

--- app.js
+++ app.js
@@ -8,12 +8,62 @@ console.log("EXERCISE 1:\n==========\n");
 
 // YOUR CODE HERE
 
+let aa;
+let b;
+
+aa = 20;
+b = 4;
+
+let add;
+add = aa + b;
+
+let minus;
+minus = aa - b;
+
+let multiply;
+multiply = aa * b;
+
+let dividing;
+dividing = aa / b;
+
+console.log(aa,b)
+console.log(add,minus,multiply,dividing)
+
+
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
 // YOUR CODE HERE
+let a = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Jackie";
+let lastName = "Chan";
 
+console.log( a + str )
+console.log( a + str2)
+console.log( a + isPresent)
+console.log( a + firstName )
+console.log( a + lastName)
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
 // YOUR CODE HERE
+let d = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+let firstName2 = "Robin";
+let lastName2 = "Williams";
+
+console.log(d==str3)
+console.log(d===str3)
+console.log(d==str4)
+console.log(!isPresent2)
+console.log("eleven" == str4 && d >= str3)
+console.log(!isPresent2 || isPresent)
+console.log(0 == false)
+console.log(0 === false)
+console.log(0 != false)
+console.log(0 !== false)
